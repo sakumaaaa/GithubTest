f15b12b63f8bce314b60c3374ab2a9e943728ae1
sakumaaaa <kuma817shuu@gmail.com>
2023-05-04 13:27:01 +0900

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by asaken_n44 on 2023/05/04.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
